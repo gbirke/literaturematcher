0a97b5d856ee853d77dda25504df750155ded5b7
Gabriel Birke <gabriel.birke@posteo.net>
2022-08-09 10:14:11 +0200

    Add vue app for rendering

---
+++ app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import entries from './literature.json';
+
+</script>
+
+<template>
+	<main>
+		{{entries.length}} Einträge
+		<table>
+			<tr v-for="entry in entries" :key="entry.lineNumber">
+				<td>{{entry.manualEntry}}
+				</td>
+				<td>{{entry.zoteroEntry}}</td>
+			</tr>
+		</table>
+
+  </main>
+</template>
+
+<style scoped>
+	tr {
+		vertical-align: top;
+	}
+</style>
